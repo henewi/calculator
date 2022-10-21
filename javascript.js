0ddcb0de9946bad106e2b28e2fc3d0d690867414
henewi <hpdunn2002@gmail.com>
2022-10-21 23:47:47 +0100

    github link added

--- javascript.js
+++ javascript.js
@@ -9,3 +9,11 @@ function clear() {
 // buttons // 
 // read the click from the button
 // put that on the screen
+
+const numBtn = document.querySelectorAll('btn')
+numBtn.addEventListener('click', () => place(button.textContent))
+function place(number) {
+    if (screencontent.textContent === 0)
+        clear()
+screencontent.textContent += number
+}
