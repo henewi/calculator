19a2a4ab305fad0efaf5e02a25bba71aa9fd3614
henewi <hpdunn2002@gmail.com>
2022-10-22 13:32:58 +0100

    favicon added and css html corrections

--- javascript.js
+++ javascript.js
@@ -10,10 +10,3 @@ function clear() {
 // read the click from the button
 // put that on the screen
 
-const numBtn = document.querySelectorAll('btn')
-numBtn.addEventListener('click', () => place(button.textContent))
-function place(number) {
-    if (screencontent.textContent === 0)
-        clear()
-screencontent.textContent += number
-}
