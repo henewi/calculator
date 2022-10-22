31525e9a14d367b61e0757232835a690ff9b5851
henewi <hpdunn2002@gmail.com>
2022-10-22 19:30:50 +0100

    some js additions

--- javascript.js
+++ javascript.js
@@ -1,4 +1,4 @@
-// clear button //
+// Clear button //
 const clearBtn = document.getElementById('clearBtn')
 const screencontent = document.getElementById('screencontent')
 clearBtn.addEventListener('click', clear)
@@ -6,10 +6,40 @@ function clear() {
     screencontent.textContent=''
 }
 
-// buttons // 
-// read the click from the button
-// put that on the screen
+// Main button workings // 
 
 const numberButtons = document.querySelectorAll('[data-number]')
 const operationButtons = document.querySelectorAll('[data-operation')
-const equalsButton = document.querySelector
+const equalsButton = document.querySelector('[data-equals]')
+const previousOperandTextElement = document.querySelector('[data-previous-operand]')
+const currentOperandTextElement = document.querySelector('[data-current-operand]')
+
+class Calculator {
+    constructor(previousOperandTextElement, currentOperandTextElement) {
+        this.previousOperandTextElement = previousOperandTextElement
+        this.currentOperandTextElement = currentOperandTextElement
+        this.clear()
+    }    
+}
+
+chooseOperation(operation) { 
+}
+
+appendNumber(number) {
+}
+
+compute() {
+}
+
+updateDisplay() {
+    this.currentOperandTextElement.innerText = this.currentOperand
+}
+
+const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement)
+
+numberButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.appendNumber(button.innerText)
+        calculator.updateDisplay
+    })
+})
